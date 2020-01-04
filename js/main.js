92cadfeedd657a7d3e5dbf66176fe701790cdb17
Cheikh-Oumar DIALLO <codiallo19@gmail.com>
2020-01-03 22:12:45 -0500

    updqte

--- js/main.js
+++ js/main.js
@@ -63,7 +63,17 @@
 
 	///////////////////////////
 	// Owl Carousel
-
+	// Owl Carousel
+	$('#about-slider').owlCarousel({
+		items:1,
+		loop:true,
+		margin:15,
+		nav: true,
+		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		dots : true,
+		autoplay : true,
+		animateOut: 'fadeOut'
+	});
 	// Owl Carousel
 	$('#about-slider2').owlCarousel({
 		items:2,
